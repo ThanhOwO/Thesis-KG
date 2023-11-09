d20ee0625b084e8c28550bdb0862dc1ad2c91639
Thanh <91793770+ThanhOwO@users.noreply.github.com>
2023-11-09 21:22:46 +0700

    add bot response (test 1)

--- chatbot_client/src/App.jsx
+++ chatbot_client/src/App.jsx
@@ -1,50 +1,12 @@
 // src/App.js
 import React from 'react';
 import './App.scss';
-import gptlogo from './assets/chatgpt.svg'
-import addBtn from './assets/add-30.png'
-import msgIcon from './assets/message.svg'
-import home from './assets/home.svg'
-import saved from './assets/bookmark.svg'
-import rocket from './assets/rocket.svg'
-import sendBtn from './assets/send.svg' 
-import userIcon from './assets/user-icon.png'
-import gptImgLogo from './assets/chatgptLogo.svg'
+import { IntegrateUI } from './components';
+
 
 function App() {
   return (
-    <div className='App'>
-      <div className='sideBar'>
-          <div className='upperSide'>
-            <div className='upperSideTop'><img src={gptlogo} alt='Logo' className='logo'/><span className='brand'>My Chatbot</span></div>
-            <button className='midBtn'><img src={addBtn} alt='new chat' className='addBtn'/> New Chat</button>
-            <div className='upperSideBottom'>
-              <button className='query'><img src={msgIcon} alt='Query'/>Hello how can I help you ?</button>
-            </div>
-          </div>
-          <div className='lowerSide'>
-            <div className='listItems'><img src={home} alt='' className='listItemsImg'/>Home</div>
-            <div className='listItems'><img src={saved} alt='' className='listItemsImg'/>Save</div>
-            <div className='listItems'><img src={rocket} alt='' className='listItemsImg'/>Upgrade to Pro</div>
-          </div>
-      </div>
-      <div className='main'>
-        <div className='chats'>
-          <div className='chat'>
-            <img className='chatImg' src={userIcon} alt='' /><p className='txt'>Hi this is a test message</p>
-          </div>
-          <div className='chat bot'>
-            <img className='chatImg' src={gptImgLogo} alt='' /><p className='txt'>Loremsdoashdlndakjsldh saodhsajkldhsakjdhsa lkjsahdoiahfoiashd hdoisadhasoidhisldh chxzlkcnzxlkchjzxckl lkxzjckxzlcjzxklc lkjxzclkxzjcklxzcjxzklcjzxlk cxzlkcjzxkl czlxkjclkzx cjzlkxcjzxlk czxklcjzlkcj dsadddddddsadsadsad dssadasdsadfasdxcxzc xzc asdasd adac czx cqwdf c faasdasdasasd czxc sad </p>
-          </div>
-        </div>
-        <div className='chatFooter'>
-          <div className='inp'>
-            <input type='text' placeholder='Send a message...'/><button className='send'><img src={sendBtn} alt='Send'/></button>
-          </div>
-          <p>My Chat can make mistakes. Consider checking important information.</p>
-        </div>
-      </div>
-    </div>
+    <IntegrateUI/>
   );
 }
 
---
+++ chatbot_client/src/components/Integrate/index.js
@@ -0,0 +1,3 @@
+/** @format */
+
+export { default as IntegrateUI } from './IntegrateUI.jsx'
