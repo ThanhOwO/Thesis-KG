eeedccc4e81146c276a9b856faf760d0d4a12f0c
Thanh <91793770+ThanhOwO@users.noreply.github.com>
2023-11-20 12:34:26 +0700

    add/fix BERT NER

--- server/ner.py
+++ server/ner.py
@@ -1,13 +1,39 @@
 import sys
-import spacy
 import json
+from bert import Ner
 
 def main():
-    nlp = spacy.load("./model-last")
-    text = sys.argv[1]
-    doc = nlp(text)
-    entities = [{"text": ent.text, "label": ent.label_} for ent in doc.ents]
-    print(json.dumps(entities))
+    try:
+        model = Ner("./out_base")
+        text = sys.argv[1]
+        output = model.predict(text)
+        phrases = []
+        current_tag = None
+        current_phrase = []
+
+        for word_info in output:
+            tag = word_info['tag']
+            word = word_info['word']
+            
+            if tag.startswith('B-'):
+                if current_phrase:
+                    phrases.append({'text': ' '.join(current_phrase), 'label': current_tag[2:]})
+                    current_phrase = []
+                
+                current_tag = tag
+            if tag == f"I-{current_tag.split('-')[-1]}" or tag == current_tag:
+                current_phrase.append(word)
+
+        # Adding the last phrase
+        if current_phrase:
+            phrases.append({'text': ' '.join(current_phrase), 'label': current_tag[2:]})
+
+        # Display the output in a JSON-like format
+        print(json.dumps(phrases))
+
+    except Exception as e:
+        # If an exception occurs, print an empty array
+        print(json.dumps(['']))
 
 if __name__ == "__main__":
     main()
